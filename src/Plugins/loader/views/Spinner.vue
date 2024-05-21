59f6bfe221ae91322133334be0b4e2081de0f9d1
Rick <derickharon6@gmail.com>
2024-05-22 00:06:20 +0300

    added global loader

---
+++ src/Plugins/loader/views/Spinner.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-overlay
+      v-if="globalStore.globalGetter['loading']"
+      z-index="10"
+      color="#000000"
+      opacity=".4"
+    >
+      <spinner :animation-duration="2500" :size="65" />
+    </v-overlay>
+  </template>
+  
+  <script>
+  import { HalfCircleSpinner as spinner } from "epic-spinners";
+  import { useGlobalStore } from "@/store";
+  export default {
+    setup() {
+    const globalStore = useGlobalStore();
+    return { globalStore };
+  },
+    name: "Spinner",
+    components: {
+      spinner,
+    },
+  };
+  </script>
+  
+  <style scoped></style>
+  
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import VueTelInputVuetify from 'vue-tel-input-vuetify/lib';
+import Vue3Toasity from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
 import {pinia} from "./Plugins/pinia";
 import auth from "./modules/Auth";
 // import home from "./modules/home";
@@ -18,5 +20,13 @@ app.use(auth, options);
 // app.use(landing, options);
 
 const app = createApp(App)
+app.use(Vue3Toasity, {
+    autoClose: 2000,
+    style: {
+      opacity: "1",
+      userSelect: "initial",
+    },
+  });
+  
 app.use(pinia)
 app.use(vuetify).use(router).use(VueTelInputVuetify).mount('#app')
