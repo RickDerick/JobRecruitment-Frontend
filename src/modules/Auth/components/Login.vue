c6a391cd06fa57d0eec7b1ceb9555bcfb0fef00e
Rick <derickharon6@gmail.com>
2024-06-03 11:04:25 +0300

    updates on login

--- src/modules/Auth/components/Login.vue
+++ src/modules/Auth/components/Login.vue
@@ -1,43 +1,42 @@
 <template>
-    <v-container class="fill-height d-flex flex-column align-content-center">
-        <v-dialog v-model="dialog" persistent>
-            <v-overlay opacity="0" class="black--text" absolute>
-                <v-row style="width: 90vw">
-                    <v-col cols="12" md="4" offset-md="4">
-                        <v-card
-                        title="Login to account"
-                        >
-                        <v-card-text class="mt-5">
-                            <v-form v-model="valid" ref="loginForm">
-                                 <!--User name-->
-                                  <v-text-field
-                                    @keyup.enter.prevent="login"
-                                    ref="email"
-                                    label="Email address"
-                                    :rules="rules.email"
-                                    v-model="formData.email"
-                                    variant="outlined"
-                                    placeholder="Enter email address"
-                                  />
+  <v-container class="fill-height d-flex flex-column align-content-center">
+    <v-dialog v-model="dialog" persistent>
+      <v-overlay opacity="0" class="black--text" absolute>
+        <v-row style="width: 90vw">
+          <v-col cols="12" md="4" offset-md="4">
+            <v-card tile light>
+              <v-card-title> Login to account </v-card-title>
 
-                                  <!-- Password -->                                       
-                                  <v-text-field
-                                    @keyup.enter.prevent="login"
-                                    ref="password"
-                                    :rules="rules.password"
-                                    label="Password"
-                                    v-model="formData.password"
-                                    variant= "outlined"
-                                    placeholder="Enter Password"
-                                    :append-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
-                                    :type="showPassword ? 'text' : 'password'"
-                                    @click:append-inner="showPassword = !showPassword"
-                                  />
+              <v-card-text class="mt-5">
+                <v-form ref="loginForm" v-model="isValid">
+                  <!--User name-->
+                  <v-text-field
+                    @keyup.enter.prevent="login"
+                    ref="email"
+                    label="Email address"
+                    :rules="rules.email"
+                    v-model="formData.email"
+                    outlined
+                    placeholder="Enter email address"
+                  />
 
-                            </v-form>
+                  <!-- Password -->
+                  <v-text-field
+                    @keyup.enter.prevent="login"
+                    ref="password"
+                    :rules="rules.password"
+                    label="Password"
+                    v-model="formData.password"
+                    outlined
+                    placeholder="Enter Password"
+                    :append-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+                    :type="showPassword ? 'text' : 'password'"
+                    @click:append="showPassword = !showPassword"
+                  />
+                </v-form>
+              </v-card-text>
 
-                        </v-card-text>
-                        <v-card-actions class="mt-n5 justify-center">
+              <v-card-actions class="mt-n5 justify-center">
                 <v-col cols="12">
                   <v-btn @click="login" color="primary" class="mt-n5" block>
                     Login
@@ -61,7 +60,7 @@
                     register
                   </v-btn>
 
-                  <!-- <p class="text-center overline grey--text">or</p>
+                  <p class="text-center overline grey--text">or</p>
 
                   <v-btn
                     @click="linkedinLogin"
@@ -70,34 +69,50 @@
                     class="mt-n3 white--text"
                   >
                     signin with LinkedIn
-                  </v-btn> -->
+                  </v-btn>
                 </v-col>
               </v-card-actions>
-                        </v-card>
-                    </v-col>
-                </v-row>
-            </v-overlay>
-        </v-dialog>
-    </v-container>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-overlay>
+
+      <request-access />
+    </v-dialog>
+  </v-container>
 </template>
+
 <script>
-export default{
-    name:"login",
-    components:{},
+import RequestAccess from "@/modules/auth/components/requestAccess";
+import { useAuthStore } from "@/store/auth";
+export default {
+  name: "login",
+  components: { RequestAccess },
 
-    data: ()=>{
-        return{
-            dialog:false,
-            valid:false,
-            showPassword: false,
+  setup(){
+      const authStore = useAuthStore();
+      // const globalStore = useGlobalStore();
 
-            formData:{
-                email: "",
-                password:"",
-                token:"",
+    },
 
-            },
-            rules: {
+  data: function () {
+    return {
+      dialog: true,
+      token: "",
+      test: [],
+      isValid: false,
+      isValidSearch: false,
+      isValidPassword: false,
+      showPassword: false,
+      step: "login",
+      steps: ["login", "verify", "password"],
+      memberNo: "",
+      formData: {
+        email: "",
+        password: "",
+        token: "",
+      },
+      rules: {
         email: [
           (value) => {
             const pattern =
@@ -107,7 +122,142 @@ export default{
         ],
         required: [(value) => !!value || "Required."],
       },
-        }
+      viewPassword: false,
+    };
+  },
+
+  beforeRouteEnter(to, from, next) {
+    next((v) => {
+      if (v.$route.query.memberNo && v.$route.query.voterToken) {
+        v.token = v.$route.query.voterToken;
+        v.memberNo = v.$route.query.memberNo;
+        v.step = "password";
+      }
+    });
+  },
+
+  mounted() {
+    Event.$on("search-success", () => {
+      this.step = "verify";
+    });
+
+    Event.$on("search-failed", (message) => {
+      this.$alert({
+        title: "Voter not found",
+        text: message,
+      });
+    });
+
+    Event.$on("token-verification-success", () => {
+      this.step = "password";
+      this.dialog = false;
+    });
+
+    Event.$on("token-verification-failed", (message) => {
+      this.$alert({
+        title: "Invalid voter token",
+        text: message,
+      });
+    });
+
+    Event.$on("token-sending-failed", (message) => {
+      this.$alert({
+        title: "Token not sent",
+        text: message,
+      });
+    });
+  },
+
+  computed: {
+
+    routeParams() {
+      return this.$route.params.code;
     },
-}
+  },
+
+  methods: {
+    login: function () {
+      this.$refs.loginForm.validate();
+      if (this.isValid) {
+        this.authStore.login(this.formData);
+      }
+    },
+    linkedinLogin: function () {
+      this.$store.dispatch("Auth/linkedinLogin");
+    },
+
+    search: function () {
+      this.$refs.searchForm.validate();
+      if (this.isValidSearch) {
+        this.$store.dispatch("Auth/search", { memberNo: this.memberNo });
+      }
+    },
+
+    setPassword: function () {
+      this.$refs.passwordForm.validate();
+      if (this.isValidPassword) {
+        this.$store.dispatch("Auth/setPassword", {
+          ...this.formData,
+          voterToken: this.token,
+        });
+      }
+    },
+
+    sendToken: function (type) {
+      this.$store.dispatch("Auth/sendToken", {
+        user: { ...this.results },
+        type: type,
+      });
+    },
+
+    verifyToken: function () {
+      this.$store.dispatch("Auth/verifyToken", {
+        user: { ...this.results },
+        voterToken: this.token,
+      });
+    },
+
+    emailMask: function (email) {
+      if (!email) return "";
+
+      const index = email.lastIndexOf("@");
+      const prefix = email.substring(0, index);
+      const postfix = email.substring(index);
+
+      const mask = prefix
+        .split("")
+        .map(function (o, i) {
+          if (i === 0 || i === index - 1) {
+            return o;
+          } else {
+            return "*";
+          }
+        })
+        .join("");
+
+      return mask + postfix;
+    },
+
+    phoneMask: function (phoneNumber) {
+      return phoneNumber ? phoneNumber.replace(/\d{5}$/, "*****") : "";
+    },
+  },
+  watch: {
+    getUserData: {
+      handler: function () {
+        if (this.getUserData.length > 0) {
+          this.test = this.getUserData;
+        }
+      },
+    },
+  },
+};
 </script>
+
+<style scoped>
+@import url("../styles.css");
+
+.v-input__append-outer {
+  margin-top: 8px;
+}
+</style>
--- src/stores/constants.js
+++ src/stores/constants.js
@@ -1,7 +1,8 @@
 export default {
     // AUTH CONSTANTS
-
+    login: `auth/login`,
     sendOtp: `auth/resend-otp`,
     passwordReset: `auth/forgot-password`,
     verifyOtp: `auth/verify-otp`,
+    linkedinLogin: `auth/login/linkedin-openid`,
 }
